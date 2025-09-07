a6248617a7174ce771d96ee1f03e4b21a020593e
bethelehem12 <verolina955@gmail.com>
2025-09-07 18:18:58 -0400

    Re-add request file

---
+++ netflix-clone/src/utils/requests.jsx
@@ -0,0 +1,15 @@
+const API_KEY = process.env.VITE_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchScienceFictionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchThrillerMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+  fetchWarMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10752`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchCrimeMovies: `/discover/movie?api_key=${API_KEY}&with_genres=80`,
+};
+
+export default requests;
