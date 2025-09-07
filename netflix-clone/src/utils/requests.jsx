8b969d1c5c5a42e1fec537c122808e3e1755dc8f
bethelehem12 <verolina955@gmail.com>
2025-09-07 17:45:19 -0400

    removed comments from request file

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,7 +1,5 @@
 const API_KEY = process.env.VITE_API_KEY;
 
-// "282a5343424e0e7235395a4ae497b0f5";
-
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -15,5 +13,3 @@ const requests = {
 };
 
 export default requests;
-
-// https://api.themoviedb.org/3/trending/all/week?api_key=282a5343424e0e7235395a4ae497b0f5&language=en-US
